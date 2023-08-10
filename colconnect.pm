5c0dc6662a1d519f18b00c46737d067477d3ed7e
Toshiyuki SHIMONO <bin4tsv@gmail.com>
2023-08-10 21:23:44 +0900

    最初の7個のファイルを用意。プログラムファイルcolconnect は多分満足に動く。

---
+++ colconnect.pm
@@ -0,0 +1,37 @@
+package App::colconnect ;  
+our $VERSION = '0.020' ; 
+our $DATE = '2023-08-10T19:20+09:00' ; 
+
+=encoding utf8
+
+=head1 NAME
+
+App::colconnect -- 
+
+=head1 SYNOPSIS
+
+This module provides a Unix-like command `F<colconnect>'. 
+
+=head1 DESCRIPTION
+
+ colconnect [FILENAME]
+
+ Options : 
+
+=head1 SEE ALSO
+
+
+
+=head1 AUTHOR
+
+下野寿之 Toshiyuki SHIMONO <bin4tsv@gmail.com> The Institute of Statistical Mathematics, a visiting researcher. 
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2023 Toshiyuki SHIMONO. All rights reserved.
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=cut
+
+1 ;
